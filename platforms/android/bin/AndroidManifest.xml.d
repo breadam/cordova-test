97efab1f8acab2fe4a621dde8eb5656f74cb411a
breadam <bilgi@breadam.com>
2015-01-09 00:15:36 +0200

    Signed-off-by: breadam <bilgi@breadam.com>

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\AndroidManifest.xml : \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes.dex : \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\BuildConfig.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\HelloCordova.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\R$attr.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\R$drawable.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\R$layout.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\R$string.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\R$xml.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes\io\cordova\hellocordova\R.class \
+C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\dexedLibs\cordova-3.0.0-1d29e49cdbdf18915c90a4cb0f3556e8.jar \
