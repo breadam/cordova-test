b9d201db09252a45784c6d419de5c4965a0bffd1
breadam <bilgi@breadam.com>
2015-01-09 00:38:57 +0200

    Signed-off-by: breadam <bilgi@breadam.com>

--- platforms/android/bin/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\HelloCordova-debug-unaligned.apk : \
-C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\HelloCordova.ap_ \
-C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\bin\classes.dex \
-C:\devel\projects\angular-react-cordova-bench\cordova-bench\platforms\android\libs\cordova-3.0.0.jar \
